66bb790418f63676150d406eb4da369978a1ffea
vladd-1 <abhaytiwari190703@gmail.com>
2026-01-10 18:48:06 +0530

    Clean workspace: Remove unrelated projects and directories

--- crypto/run_bot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Simple launcher script for the trading bot
-# Uses system Python which has all dependencies
-
-cd "$(dirname "$0")"
-
-# Run with system Python
-python3 trading_bot.py "$@"
